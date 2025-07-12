91afdd5cfb03a1117b2fa0ff871fd8a3987d338b
FelipeDuan <felipeduan1@gmail.com>
2025-07-12 00:43:32 -0300

    feat: create Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Text } from "./components/ui/text";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl text-blue-500">O famoso Hello World!</h1>
+      <div className="space-y-4">
+        <h1>Testando componentes</h1>
+
+        <div className="flex flex-col gap-2">
+          <Text variant="heading" color="primary">
+            Texto título
+          </Text>
+          <Text variant="subheading" color="danger">
+            Texto subtítulo
+          </Text>
+          <Text variant="bodyBold">Texto padrão em negrito</Text>
+          <Text>Texto padrão</Text>
+          <Text variant="small" color="muted">
+            Texto pequeno
+          </Text>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/ui/text.tsx
@@ -0,0 +1,55 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+export const textVariants = cva("font-sans", {
+  variants: {
+    variant: {
+      heading: "text-2xl font-bold",
+      subheading: "text-xl font-semibold",
+      body: "text-base font-normal",
+      bodyBold: "text-base font-semibold",
+      small: "text-sm font-normal",
+    },
+    color: {
+      default: "text-gray-900",
+      muted: "text-gray-500",
+      primary: "text-blue-600",
+      danger: "text-red-600",
+    },
+    align: {
+      left: "text-left",
+      center: "text-center",
+      rigth: "text-right",
+    },
+  },
+  defaultVariants: {
+    variant: "body",
+    color: "default",
+    align: "left",
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: String;
+  children?: React.ReactNode;
+}
+
+export function Text({
+  as = "span",
+  variant,
+  color,
+  align,
+  className,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: cx(textVariants({ variant, color, align }), className),
+      ...props,
+    },
+    children
+  );
+}
