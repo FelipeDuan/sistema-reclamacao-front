43db57040367a9948a10f8383b49cd3b4d7f4428
FelipeDuan <felipeduan1@gmail.com>
2025-07-12 20:41:30 -0300

    feat: iniciando configuracao das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 import { PageLogin } from "./pages/page-login";
+import { PageComponents } from "./pages/page-components";
 
 export default function App() {
   return (
-    <>
-      <PageLogin />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<PageLogin />} />
+
+        <Route index element={<PageComponents />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
