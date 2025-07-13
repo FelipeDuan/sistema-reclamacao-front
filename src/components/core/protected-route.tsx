87bdd1ddaba40cf186d61d0549ba3815254c43a2
FelipeDuan <felipeduan1@gmail.com>
2025-07-12 21:55:10 -0300

    feat: protectedRoute component + fetches auxiliares para a api e token

---
+++ src/components/core/protected-route.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { verifyToken } from "../../helpers/api";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function check() {
+      const isValid = await verifyToken();
+
+      if (!isValid) {
+        alert("Seu token está inválido! Por favor, faça login novamente.");
+        navigate("/login", { replace: true });
+      }
+
+      // ToDo: seria uma boa eu implementar um toast depois, lembrar disso aqui mais tarde.
+    }
+
+    check();
+  }, [navigate]);
+
+  return <>{children}</>;
+}
--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,10 +1,13 @@
-interface PublicFetchProps {
+export const BASE_URL = "http://localhost:8080";
+export const TOKEN_KEY = "reclame.token";
+
+interface FetchProps {
   url: string;
   options?: RequestInit;
 }
 
-export async function publicFetch({ url, options }: PublicFetchProps) {
-  const response = await fetch(url, {
+export async function publicFetch({ url, options }: FetchProps) {
+  const res = await fetch(url, {
     ...options,
     headers: {
       "Content-Type": "application/json",
@@ -12,12 +15,54 @@ export async function publicFetch({ url, options }: PublicFetchProps) {
     },
   });
 
-  if (!response.ok) {
+  if (!res.ok) {
     throw (
-      new Error(await response.text()) ||
-      "Erro ao fazer requisição nessa rota pública"
+      new Error(await res.text()) ||
+      "Erro ao fazer requisição nessa rota pública."
     );
   }
 
-  return response.json();
+  return res.json();
+}
+
+export async function authFetch({ url, options }: FetchProps) {
+  const token = localStorage.getItem(TOKEN_KEY);
+
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      ...(options?.headers || {}),
+    },
+  });
+
+  if (res.status === 401 || res.status === 403) {
+    localStorage.removeItem(TOKEN_KEY);
+    window.location.href = "/login";
+    throw new Error("Sessão expirada. Faça login novamente.");
+  }
+
+  if (!res.ok) {
+    throw new Error((await res.text()) || "Erro na requisição autenticada.");
+  }
+}
+
+export async function verifyToken() {
+  try {
+    const res = await fetch(`${BASE_URL}/api/reclamacoes/hello`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("Token inválido");
+    }
+
+    return true;
+  } catch {
+    localStorage.removeItem(TOKEN_KEY);
+    return false;
+  }
 }
