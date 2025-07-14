45fa33601a1a2b65b073600ae9b00efdd2a4dcf2
FelipeDuan <felipeduan1@gmail.com>
2025-07-14 18:49:44 -0300

    refact: create useEditReclamacoes

--- src/components/core/edit-reclamacao-form.tsx
+++ src/components/core/edit-reclamacao-form.tsx
@@ -1,53 +1,26 @@
-import { useEffect, useState } from "react";
-import { authFetch, BASE_URL, handleApiError } from "../../helpers/api";
-import { useNavigate, useParams } from "react-router";
+import { useEffect } from "react";
 import { Card } from "../ui/card";
 import { Text } from "../ui/text";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
+import { useEditReclamacao } from "../../hooks/use-edit-reclamacao";
 
 export function EditReclamacaoForm() {
-  const { id } = useParams();
-  const [titulo, setTitulo] = useState("");
-  const [descricao, setDescricao] = useState("");
-  const navigate = useNavigate();
+  const {
+    id,
+    titulo,
+    descricao,
+    fetchReclamacao,
+    handleEditDescricao,
+    handleEditTitulo,
+    handleSubmit,
+  } = useEditReclamacao();
 
   useEffect(() => {
-    async function fetchReclamacao() {
-      try {
-        const data = await authFetch({
-          url: `${BASE_URL}/api/reclamacoes/${id}`,
-        });
-        setTitulo(data.titulo);
-        setDescricao(data.descricao);
-      } catch (error: any) {
-        alert(handleApiError(error, "Erro ao carregar reclamação."));
-      }
-    }
-
     fetchReclamacao();
   }, [id]);
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-
-    try {
-      await authFetch({
-        url: `${BASE_URL}/api/reclamacoes/${id}`,
-        options: {
-          method: "PUT",
-          body: JSON.stringify({ titulo, descricao }),
-        },
-      });
-
-      alert("Reclamação atualizada com sucesso!");
-      navigate(`/reclamacao/${id}`, { replace: true });
-    } catch (error: any) {
-      alert(handleApiError(error, "Erro ao atualizar reclamação."));
-    }
-  }
 
   return (
     <Card className="p-6 max-w-5xl mx-auto w-full flex flex-col gap-6">
@@ -58,7 +31,7 @@ export function EditReclamacaoForm() {
           <Label required>Título</Label>
           <Input
             value={titulo}
-            onChange={(e) => setTitulo(e.target.value)}
+            onChange={handleEditTitulo}
             placeholder="Título"
           />
         </div>
@@ -67,7 +40,7 @@ export function EditReclamacaoForm() {
           <Label required>Descrição</Label>
           <Textarea
             value={descricao}
-            onChange={(e) => setDescricao(e.target.value)}
+            onChange={handleEditDescricao}
             placeholder="Descrição"
             className="h-40"
           />
--- src/hooks/use-edit-reclamacao.ts
+++ src/hooks/use-edit-reclamacao.ts
@@ -1,1 +1,59 @@
-export function useEditReclamacao() {}
+import { useState, type ChangeEvent } from "react";
+import { useNavigate, useParams } from "react-router";
+import { authFetch, BASE_URL, handleApiError } from "../helpers/api";
+
+export function useEditReclamacao() {
+  const { id } = useParams();
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const navigate = useNavigate();
+
+  async function fetchReclamacao() {
+    try {
+      const data = await authFetch({
+        url: `${BASE_URL}/api/reclamacoes/${id}`,
+      });
+      setTitulo(data.titulo);
+      setDescricao(data.descricao);
+    } catch (error: any) {
+      alert(handleApiError(error, "Erro ao carregar reclamação."));
+    }
+  }
+
+  function handleEditTitulo(e: ChangeEvent<HTMLInputElement>) {
+    setTitulo(e.target.value);
+  }
+
+  function handleEditDescricao(e: ChangeEvent<HTMLTextAreaElement>) {
+    setDescricao(e.target.value);
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      await authFetch({
+        url: `${BASE_URL}/api/reclamacoes/${id}`,
+        options: {
+          method: "PUT",
+          body: JSON.stringify({ titulo, descricao }),
+        },
+      });
+
+      alert("Reclamação atualizada com sucesso!");
+      navigate(`/reclamacao/${id}`, { replace: true });
+    } catch (error: any) {
+      alert(handleApiError(error, "Erro ao atualizar reclamação."));
+    }
+  }
+
+  return {
+    id,
+    titulo,
+    descricao,
+    fetchReclamacao,
+    handleEditTitulo,
+    handleEditDescricao,
+    handleSubmit,
+  };
+}
