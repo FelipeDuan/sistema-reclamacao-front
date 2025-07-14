f862a3f7ad5cf3fac0be18ef180d9dc512d9942e
FelipeDuan <felipeduan1@gmail.com>
2025-07-14 12:30:04 -0300

    refact: criar reclamacao

--- src/components/core/reclamacao-form.tsx
+++ src/components/core/reclamacao-form.tsx
@@ -3,59 +3,17 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
-import { useState, type ChangeEvent } from "react";
-import { authFetch, BASE_URL } from "../../helpers/api";
-import { useNavigate } from "react-router";
+import { useCreateReclamacao } from "../../hooks/use-create-reclamacao";
 
 export function ReclamacaoForm() {
-  const [titulo, setTitulo] = useState("");
-  const [descricao, setDescricao] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
-
-  function handleTitulo(e: ChangeEvent<HTMLInputElement>) {
-    setTitulo(e.target.value);
-  }
-
-  function handleDescricao(e: ChangeEvent<HTMLTextAreaElement>) {
-    setDescricao(e.target.value);
-  }
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-
-    setIsLoading(true);
-
-    try {
-      const formData = {
-        titulo,
-        descricao,
-      };
-
-      const data = await authFetch({
-        url: `${BASE_URL}/api/reclamacoes`,
-        options: {
-          method: "POST",
-          body: JSON.stringify(formData),
-        },
-      });
-
-      if (data && data.id) {
-        setTitulo("");
-        setDescricao("");
-        setIsLoading(false);
-        alert("Reclamação criada com sucesso!");
-        navigate("/", { replace: true });
-        return;
-      }
-
-      alert("Não foi possível criar reclamação");
-    } catch (error: any) {
-      alert(error.message || "Erro ao criar nova reclamação.");
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  const {
+    titulo,
+    descricao,
+    isLoading,
+    handleTitulo,
+    handleDescricao,
+    handleSubmit,
+  } = useCreateReclamacao();
 
   return (
     <>
--- src/hooks/use-create-reclamacao.tsx
+++ src/hooks/use-create-reclamacao.tsx
@@ -1,1 +1,63 @@
-export function useCreateReclamacao() {}
+import { useState, type ChangeEvent } from "react";
+import { useNavigate } from "react-router";
+import { authFetch, BASE_URL } from "../helpers/api";
+
+export function useCreateReclamacao() {
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handleTitulo(e: ChangeEvent<HTMLInputElement>) {
+    setTitulo(e.target.value);
+  }
+
+  function handleDescricao(e: ChangeEvent<HTMLTextAreaElement>) {
+    setDescricao(e.target.value);
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      const formData = {
+        titulo,
+        descricao,
+      };
+
+      const data = await authFetch({
+        url: `${BASE_URL}/api/reclamacoes`,
+        options: {
+          method: "POST",
+          body: JSON.stringify(formData),
+        },
+      });
+
+      if (data && data.id) {
+        setTitulo("");
+        setDescricao("");
+        setIsLoading(false);
+        alert("Reclamação criada com sucesso!");
+        navigate("/", { replace: true });
+        return;
+      }
+
+      alert("Não foi possível criar reclamação");
+    } catch (error: any) {
+      alert(error.message || "Erro ao criar nova reclamação.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return {
+    titulo,
+    descricao,
+    isLoading,
+    handleTitulo,
+    handleDescricao,
+    handleSubmit,
+  };
+}
